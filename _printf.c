cff83a4cbb51d0915b59d1a9add541df55cdabdb
SrxnB <serxan.bagirli@gmail.com>
2024-07-26 10:37:11 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -4,8 +4,8 @@
 /**
  *_printf - prints to output according to format
  *@format: character string
- *
- *Return: number of characters printed
+ 
+ *Return: number of characters printed.
  */
 
 int _printf(const char *format, ...)
@@ -24,7 +24,7 @@ int _printf(const char *format, ...)
 			if (format[i + 1])
 			{
 				if (format[i + 1] != 'c' && format[i + 1] != 's'
-				&& format[i + 1] != '%' && format[i + 1] != 'd'
+             			&& format[i + 1] != '%' && format[i + 1] != 'd'
 				&& format[i + 1] != 'i')
 				{
 					j += _putchar(format[i]);
